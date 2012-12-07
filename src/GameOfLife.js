3b9040f13f37281995ae3bd7e9eb1a0a1fb05b66
Nir Naor <nirnaori@gmail.com>
2012-12-07 17:16:49 +0200

    done implementing the evolve method

--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -72,4 +72,31 @@ GameOfLife.prototype.reproduced = function(x, y){
 GameOfLife.prototype.evolve = function(){
   this.cells_to_kill = new Array();
   this.cells_to_revive = new Array();
+
+  for (var i = 0; i < this.size; i += 1) {
+    for (var j = 0; j < this.size.length; j += 1) {
+      if(this.is_alive(i,j)){
+        if(this.overcrowded(i,j) || this.under_populated(i, j))
+          this.cells_to_kill.push([i, j]);
+      }
+      else
+        if(this.reproduced(i, j))
+          this.cells_to_revive([i, j]);
+    };
+  };
+
+
+  for (var i = 0; i < this.cells_to_kill.length; i += 1) {
+    var x = this.cells_to_kill[i][0];
+    var y = this.cells_to_kill[i][1];
+    this.matrix[x][y] = undefined;
+  };
+
+  for (var i = 0; i < this.cells_to_revive.length; i += 1) {
+    var x = this.cells_to_kill[i][0];
+    var y = this.cells_to_kill[i][1];
+    this.matrix[x][y] = 1;
+  };
+
+
 };
