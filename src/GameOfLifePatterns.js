46fb9841e9b031aba5783f7b714a5eb4e5509d6b
Nir Naor <nirnaori@gmail.com>
2012-12-08 15:38:05 +0200

    added spaceship

--- src/GameOfLifePatterns.js
+++ src/GameOfLifePatterns.js
@@ -1,5 +1,5 @@
 function GameOfLifePatterns(){
-  this.supported = ['blinker', 'toad'];
+  this.supported = ['blinker', 'toad', 'beacon', 'spaceship'];
 }
 
 GameOfLifePatterns.prototype.create = function(pattern){
@@ -8,17 +8,34 @@ GameOfLifePatterns.prototype.create = function(pattern){
   var result = new GameOfLife(10);
 
   if(pattern === "blinker"){
-      result.add_living_cell(3, 1);
-      result.add_living_cell(3, 2);
-      result.add_living_cell(3, 3);
+    result.add_living_cell(3, 1);
+    result.add_living_cell(3, 2);
+    result.add_living_cell(3, 3);
   }
   else if (pattern === "toad"){
-      result.add_living_cell(3, 6);
-      result.add_living_cell(4, 6);
-      result.add_living_cell(5, 6);
-      result.add_living_cell(2, 7);
-      result.add_living_cell(3, 7);
-      result.add_living_cell(4, 7);
+    result.add_living_cell(3, 6);
+    result.add_living_cell(4, 6);
+    result.add_living_cell(5, 6);
+    result.add_living_cell(2, 7);
+    result.add_living_cell(3, 7);
+    result.add_living_cell(4, 7);
+  }
+  else if (pattern === "beacon"){
+    result.add_living_cell(5, 5);
+    result.add_living_cell(6, 5);
+    result.add_living_cell(5, 6);
+
+    result.add_living_cell(8, 7);
+    result.add_living_cell(7, 8);
+    result.add_living_cell(8, 8);
+
+  }
+  else if (pattern === "spaceship"){
+    result.add_living_cell(3, 3);
+    result.add_living_cell(4, 4);
+    result.add_living_cell(2, 5);
+    result.add_living_cell(3, 5);
+    result.add_living_cell(4, 5);
   }
   else
       throw new Error("pattern is not supported yet");
