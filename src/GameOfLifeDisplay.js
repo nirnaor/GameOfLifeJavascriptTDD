8fde53018de3590ebeb745cb6def423f5cd8d638
Nir Naor <nirnaori@gmail.com>
2012-12-08 11:03:16 +0200

    building the matrix string corectly

--- src/GameOfLifeDisplay.js
+++ src/GameOfLifeDisplay.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
   console.log("hello there");
+  var patterns = new GameOfLifePatterns();
+
+
+  function draw_matrix(){
+    console.log('this is draw matrix');
+    gol_instance = patterns.create("bla");
+    draw(gol_instance.matrix);
+  };
+
+  function draw(matrix){
+    var result = "";
+    for (var i = 0; i < matrix.length; i += 1) {
+      for (var j = 0; j < matrix[i].length; j += 1) {
+        if (matrix[i][j] === 1)
+          result += "1";
+        else
+          result += "0";
+      };
+      result += "\n";
+    };
+
+    $('#game_of_life_display').text(result);
+
+  };
+
+  draw_matrix();
 });
